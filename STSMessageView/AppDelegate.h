1dc9d4219a6f2aabf269ceb7830e02bf04ab1047
Kanjiviva <stevesun0123@email.com>
2016-02-25 20:20:35 -0800

    Initial Commit

---
+++ STSMessageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STSMessageView
+//
+//  Created by Steve on 2016-02-25.
+//  Copyright © 2016 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STSMessageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STSMessageView
+//
+//  Created by Steve on 2016-02-25.
+//  Copyright © 2016 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
